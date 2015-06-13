a798fd7d949dd303825146a30cfcbd2b6d980180
EvilEntity <me@piotrjastrzebski.io>
2015-06-13 16:55:35 +0200

    added stub video test

---
+++ core/src/io/piotrjastrzebski/playground/video/VideoTest.java
@@ -0,0 +1,26 @@
+package io.piotrjastrzebski.playground.video;
+
+import io.piotrjastrzebski.playground.BaseScreen;
+
+/**
+ * Created by EvilEntity on 07/06/2015.
+ */
+public class VideoTest extends BaseScreen {
+	public VideoTest () {
+		super();
+		// TODO test gdx video
+	}
+
+	@Override public void render (float delta) {
+		super.render(delta);
+	}
+
+	@Override public void resize (int width, int height) {
+		super.resize(width, height);
+
+	}
+
+	@Override public void dispose () {
+		super.dispose();
+	}
+}
