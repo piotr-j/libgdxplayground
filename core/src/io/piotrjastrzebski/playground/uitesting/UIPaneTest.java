c61e3d447bd9d027e9a9c401ab18b509931da01c
EvilEntity <me@piotrjastrzebski.io>
2015-07-08 14:23:25 +0200

    added some dumb fling stuff

--- core/src/io/piotrjastrzebski/playground/uitesting/UIPaneTest.java
+++ core/src/io/piotrjastrzebski/playground/uitesting/UIPaneTest.java
@@ -2,6 +2,8 @@ package io.piotrjastrzebski.playground.uitesting;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kotcrab.vis.ui.widget.*;
 import io.piotrjastrzebski.playground.BaseScreen;
 import io.piotrjastrzebski.playground.PlaygroundGame;
@@ -15,13 +17,19 @@ public class UIPaneTest extends BaseScreen {
 		super(game);
 		VisTable data = new VisTable();
 		for (int i = 1; i <= 30; i++) {
-			data.add(new VisLabel("Some data " + i));
+			final VisTextButton button;
+			data.add(button = new VisTextButton("Some data " + i));
 			data.row();
+			button.addListener(new ClickListener() {
+				@Override public void clicked (InputEvent event, float x, float y) {
+					Gdx.app.log("", "Clicked " + button);
+				}
+			});
 		}
 
 		final VisScrollPane pane;
 		root.add(pane = new VisScrollPane(data)).pad(200);
-		pane.addListener( new InputListener() {
+		pane.addListener(new InputListener() {
 			@Override public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {
 				lastFocus = stage.getScrollFocus();
 				stage.setScrollFocus(pane);
@@ -31,5 +39,11 @@ public class UIPaneTest extends BaseScreen {
 				stage.setScrollFocus(lastFocus);
 			}
 		});
+		pane.addListener(new ActorGestureListener(){
+			@Override public void fling (InputEvent event, float velocityX, float velocityY, int button) {
+				Gdx.app.log("", "fling...");
+			}
+		});
+		pane.setCancelTouchFocus(false);
 	}
 }
