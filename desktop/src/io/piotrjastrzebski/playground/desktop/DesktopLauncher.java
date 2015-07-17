6737d0ea038df37a8670323c3379fa442e999a1c
PiotrJ <me@piotrjastrzebski.io>
2015-07-17 16:56:27 +0200

    use hdpi

--- desktop/src/io/piotrjastrzebski/playground/desktop/DesktopLauncher.java
+++ desktop/src/io/piotrjastrzebski/playground/desktop/DesktopLauncher.java
@@ -19,6 +19,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+		config.useHDPI = true;
 		new LwjglApplication(new PlaygroundGame(), config);
 	}
 }
