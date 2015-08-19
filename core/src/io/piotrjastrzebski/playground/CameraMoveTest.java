4600775ae2c4f9501bc8ce463baf332bc0afb776
EvilEntity <me@piotrjastrzebski.io>
2015-08-19 13:08:10 +0200

    added simple camera move test

---
+++ core/src/io/piotrjastrzebski/playground/CameraMoveTest.java
@@ -0,0 +1,54 @@
+package io.piotrjastrzebski.playground;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class CameraMoveTest extends BaseScreen {
+	Sprite sprite;
+	public CameraMoveTest (PlaygroundGame game) {
+		super(game);
+		sprite = new Sprite(new Texture("white.png"));
+		sprite.setSize(1, 1);
+	}
+
+	@Override public void render (float delta) {
+		super.render(delta);
+		gameCamera.update();
+		// use gui cam as the effect is not setup for scaled drawing
+		batch.setProjectionMatrix(gameCamera.combined);
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+	}
+
+	@Override public boolean keyDown (int keycode) {
+		switch (keycode) {
+		case Input.Keys.W:
+			gameCamera.position.add(0, 0.1f, 0);
+			break;
+		case Input.Keys.S:
+			gameCamera.position.add(0, -0.1f, 0);
+			break;
+		case Input.Keys.A:
+			gameCamera.position.add(0.1f, 0, 0);
+			break;
+		case Input.Keys.D:
+			gameCamera.position.add(-0.1f, 0, 0);
+			break;
+		case Input.Keys.UP:
+			sprite.setPosition(sprite.getX(), sprite.getY() + 0.1f);
+			break;
+		case Input.Keys.DOWN:
+			sprite.setPosition(sprite.getX(), sprite.getY() - 0.1f);
+			break;
+		case Input.Keys.LEFT:
+			sprite.setPosition(sprite.getX() + 0.1f, sprite.getY());
+			break;
+		case Input.Keys.RIGHT:
+			sprite.setPosition(sprite.getX() - 0.1f, sprite.getY());
+			break;
+		}
+		return super.keyDown(keycode);
+	}
+}
