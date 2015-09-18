04bc7942efe22543cc6d7caebf76ccda49876709
PiotrJ <me@piotrjastrzebski.io>
2015-09-18 12:49:27 +0200

    fixed circle stuff

--- core/src/io/piotrjastrzebski/playground/box2dtest/FancierRayLight.java
+++ core/src/io/piotrjastrzebski/playground/box2dtest/FancierRayLight.java
@@ -100,6 +100,8 @@ public class FancierRayLight implements RayCastCallback, QueryCallback {
 	int tmpsLen;
 	Vector2 tmp = new Vector2();
 	Vector2 tmp2 = new Vector2();
+	Vector2 tanA = new Vector2();
+	Vector2 tanB = new Vector2();
 	@Override public boolean reportFixture (Fixture fixture) {
 		Shape shape = fixture.getShape();
 		Body body = fixture.getBody();
@@ -125,32 +127,26 @@ public class FancierRayLight implements RayCastCallback, QueryCallback {
 			}
 			break;
 		case Circle:
+			Gdx.app.log("Fixture", "Circle ");
+
 			CircleShape circle = (CircleShape)shape;
 			Vector2 cp = body.getWorldPoint(circle.getPosition());
 			float r = circle.getRadius();
-			float angle = tmp.set(pos).sub(p).angle();
-			Gdx.app.log("Fixture", "Circle ");
-			// TODO this is brokenwhen circle is close to origin, need to find proper points somehow
-			tmp.set(cp.x + r * MathUtils.sinDeg(angle), cp.y - r * MathUtils.cosDeg(angle));
-			if (ignoreRadius || tmp.dst2(pos) <= radius * radius) {
-				// right edge
-				tmps.get(tmpsLen).set(tmp);
-				tmpsLen++;
-				tmps.get(tmpsLen).set(tmp2.set(tmp).rotate(cExtra).setLength(radius));
-				tmpsLen++;
-			}
-			tmp.set(cp.x + r * MathUtils.sinDeg(angle + 90), cp.y - r * MathUtils.cosDeg(angle + 90));
-			if (ignoreRadius || tmp.dst2(pos) <= radius * radius) {
+
+			if (ignoreRadius || cp.dst2(pos) <= radius * radius) {
 				// top
-				tmps.get(tmpsLen).set(tmp);
+				tmps.get(tmpsLen).set(cp).sub(pos);
 				tmpsLen++;
 			}
-			tmp.set(cp.x + r * MathUtils.sinDeg(angle + 180), cp.y - r * MathUtils.cosDeg(angle + 180));
-			if (ignoreRadius || tmp.dst2(pos) <= radius * radius) {
-				// left edge
-				tmps.get(tmpsLen).set(tmp);
+
+			if (findTangents(cp, r, pos, tanA, tanB)) {
+				tmps.get(tmpsLen).set(tanA);
 				tmpsLen++;
-				tmps.get(tmpsLen).set(tmp2.set(tmp).rotate(-cExtra).setLength(radius));
+				tmps.get(tmpsLen).set(tmp.set(tanA).rotate(cExtra).setLength(radius));
+				tmpsLen++;
+				tmps.get(tmpsLen).set(tanB);
+				tmpsLen++;
+				tmps.get(tmpsLen).set(tmp.set(tanB).rotate(-cExtra).setLength(radius));
 				tmpsLen++;
 			}
 			break;
@@ -171,6 +167,75 @@ public class FancierRayLight implements RayCastCallback, QueryCallback {
 		}
 		// we want all
 		return true;
+	}
+
+	private boolean findTangents (Vector2 center, float radius,
+		Vector2 external,
+		Vector2 oTanA, Vector2 oTanB) {
+
+		float dx = center.x - external.x;
+		float dy = center.y - external.y;
+		float d2 = dx * dx + dy * dy;
+		if (d2 < radius * radius) {
+			oTanA.setZero();
+			oTanB.setZero();
+			return false;
+		}
+		float len = (float)Math.sqrt(d2 - radius * radius);
+
+		int inters = findCircleCircleIntersection(
+			center.x, center.y, radius,
+			external.x, external.y, len,
+			oTanA, oTanB
+		);
+		return inters > 1;
+	}
+
+	private int findCircleCircleIntersection (
+		float x1, float y1, float r1,
+		float x2, float y2, float r2,
+		Vector2 outA, Vector2 outB
+	) {
+		float dx = x1 - x2;
+		float dy = y1 - y2;
+		float dist = (float)Math.sqrt(dx * dx + dy * dy);
+		if (dist > r1 + r2) {
+			// No solutions, the circles are too far apart.
+			outA.setZero();
+			outB.setZero();
+			return 0;
+		} else if (dist < Math.abs(r1 - r2)) {
+			// No solutions, one circle contains the other.
+			outA.setZero();
+			outB.setZero();
+			return 0;
+		} else if ((dist == 0) && (r1 == r2)) {
+			// No solutions, the circles coincide.
+			outA.setZero();
+			outB.setZero();
+			return 0;
+		} else {
+			// Find a and h.
+			float a = (r1 * r1 -
+				r2 * r2 + dist * dist) / (2 * dist);
+			float h = (float)Math.sqrt(r1 * r1 - a * a);
+
+			// Find P2.
+			float x3 = x1 + a * (x2 - x1) / dist;
+			float cy2 = y1 + a * (y2 - y1) / dist;
+
+			// Get the points P3.
+			outA.set(
+				x3 + h * (y2 - y1) / dist,
+				cy2 - h * (x2 - x1) / dist);
+			outB.set(
+				x3 - h * (y2 - y1) / dist,
+				cy2 + h * (x2 - x1) / dist);
+
+			// See if we have 1 or 2 solutions.
+			if (dist == r1 + r2) return 1;
+			return 2;
+		}
 	}
 
 	public void draw(ShapeRenderer renderer) {
