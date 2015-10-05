68f4950ddc613aca6b657e5db8fdcb9ad612c850
PiotrJ <me@piotrjastrzebski.io>
2015-10-05 21:02:28 +0200

    added self contained touch test

---
+++ core/src/io/piotrjastrzebski/playground/simple/SimplerTouchTest.java
@@ -0,0 +1,112 @@
+package io.piotrjastrzebski.playground.simple;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+/**
+ * Self contained test for proper touch/mouse handling
+ *
+ * Created by PiotrJ on 05/10/15.
+ */
+public class SimplerTouchTest extends ApplicationAdapter implements InputProcessor {
+	// we will use 32px/unit in world
+	public final static float SCALE = 32f;
+	public final static float INV_SCALE = 1.f/SCALE;
+	// this is our "target" resolution, not that the window can be any size, it is not bound to this one
+	public final static float VP_WIDTH = 1280 * INV_SCALE;
+	public final static float VP_HEIGHT = 720 * INV_SCALE;
+
+	private OrthographicCamera camera;
+	private ExtendViewport viewport;
+	private ShapeRenderer shapes;
+
+	@Override public void create () {
+		camera = new OrthographicCamera();
+		// pick a viewport that suits your thing, ExtendViewport is a good start
+		viewport = new ExtendViewport(VP_WIDTH, VP_HEIGHT, camera);
+		// ShapeRenderer so we can see our touch point
+		shapes = new ShapeRenderer();
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		shapes.setProjectionMatrix(camera.combined);
+		shapes.begin(ShapeRenderer.ShapeType.Filled);
+		shapes.circle(tp.x, tp.y, 0.25f, 16);
+		shapes.end();
+	}
+
+	Vector3 tp = new Vector3();
+	boolean dragging;
+	@Override public boolean mouseMoved (int screenX, int screenY) {
+		// we can also handle mouse movement without anything pressed
+//		camera.unproject(tp.set(screenX, screenY, 0));
+		return false;
+	}
+
+	@Override public boolean touchDown (int screenX, int screenY, int pointer, int button) {
+		// ignore if its not left mouse button or first touch pointer
+		if (button != Input.Buttons.LEFT || pointer > 0) return false;
+		camera.unproject(tp.set(screenX, screenY, 0));
+		dragging = true;
+		return true;
+	}
+
+	@Override public boolean touchDragged (int screenX, int screenY, int pointer) {
+		if (!dragging) return false;
+		camera.unproject(tp.set(screenX, screenY, 0));
+		return true;
+	}
+
+	@Override public boolean touchUp (int screenX, int screenY, int pointer, int button) {
+		if (button != Input.Buttons.LEFT || pointer > 0) return false;
+		camera.unproject(tp.set(screenX, screenY, 0));
+		dragging = false;
+		return true;
+	}
+
+	@Override public void resize (int width, int height) {
+		// viewport must be updated for it to work properly
+		viewport.update(width, height, true);
+	}
+
+	@Override public void dispose () {
+		// disposable stuff must be disposed
+		shapes.dispose();
+	}
+
+
+	@Override public boolean keyDown (int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyUp (int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyTyped (char character) {
+		return false;
+	}
+
+	@Override public boolean scrolled (int amount) {
+		return false;
+	}
+
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.useHDPI = true;
+		new LwjglApplication(new SimplerTouchTest(), config);
+	}
+}
