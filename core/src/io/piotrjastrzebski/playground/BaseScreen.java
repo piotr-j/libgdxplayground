a76d0f97d9d226745d19de13391e81bdaf5e9087
piotr-j <me@piotrjastrzebski.io>
2016-05-20 19:31:59 +0200

    added clear color as a field

--- core/src/io/piotrjastrzebski/playground/BaseScreen.java
+++ core/src/io/piotrjastrzebski/playground/BaseScreen.java
@@ -1,6 +1,7 @@
 package io.piotrjastrzebski.playground;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -66,8 +67,9 @@ public abstract class BaseScreen implements Screen, InputProcessor {
 
 	}
 
+	public Color clear = new Color(Color.BLACK);
 	@Override public void render (float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(clear.r, clear.g, clear.b, clear.a);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 
