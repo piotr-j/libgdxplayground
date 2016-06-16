174d26540e52d6f1c0f7dea096e75f70c58c9178
piotr-j <me@piotrjastrzebski.io>
2016-06-16 15:44:07 +0200

    vertex connections, connection overlap testing

--- core/src/io/piotrjastrzebski/playground/simple/PolygonEditorTest.java
+++ core/src/io/piotrjastrzebski/playground/simple/PolygonEditorTest.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -15,6 +16,8 @@ import io.piotrjastrzebski.playground.BaseScreen;
 import io.piotrjastrzebski.playground.GameReset;
 import io.piotrjastrzebski.playground.PlaygroundGame;
 import io.piotrjastrzebski.playground.bttests.dog.MarkTask;
+
+import java.util.Iterator;
 
 /**
  * Created by EvilEntity on 25/01/2016.
@@ -61,11 +64,13 @@ public class PolygonEditorTest extends BaseScreen {
 
 	protected static class PolygonEditor {
 		private Array<Vertex> vertices = new Array<>();
+		private Array<VertexConnection> connections = new Array<>();
 		private float gridX = 1, gridY = 1;
 		private Rectangle bounds = new Rectangle();
 		private Rectangle marginBounds = new Rectangle();
 		private float margin = 3;
 		private boolean drawGrid = true;
+		private float VERTEX_OVERLAP_EPSILON = .2f;
 
 		public void init (float x, float y, float width, float height){
 			bounds.set(x, y, width, height);
@@ -98,19 +103,47 @@ public class PolygonEditorTest extends BaseScreen {
 		}
 
 		private void connect (Vertex vertex1, Vertex vertex2) {
-			vertex1.connections.add(vertex2);
-			vertex2.connections.add(vertex1);
+			for (VertexConnection vc : vertex1.connections) {
+				if (vc.vertex1 == vertex2 || vc.vertex2 == vertex2) {
+					return;
+				}
+			}
+			VertexConnection connection = new VertexConnection(vertex1, vertex2);
+			vertex1.connections.add(connection);
+			vertex2.connections.add(connection);
+			connections.add(connection);
 		}
 
 		private void disconnect (Vertex vertex) {
-			for (Vertex other : vertex.connections) {
-				other.connections.removeValue(vertex, true);
+			for (VertexConnection vc : vertex.connections) {
+				if (vc.vertex1 == vertex) {
+					vc.vertex2.connections.removeValue(vc, true);
+				} else {
+					vc.vertex1.connections.removeValue(vc, true);
+				}
+				connections.removeValue(vc, true);
 			}
 		}
 
 		private void disconnect (Vertex vertex1, Vertex vertex2) {
-			vertex1.connections.removeValue(vertex2, true);
-			vertex2.connections.removeValue(vertex1, true);
+			Iterator<VertexConnection> it1 = vertex1.connections.iterator();
+			while (it1.hasNext()) {
+				VertexConnection vc = it1.next();
+				if (vc.vertex1 == vertex2 || vc.vertex2 == vertex2) {
+					it1.remove();
+					connections.removeValue(vc, true);
+					break;
+				}
+			}
+			Iterator<VertexConnection> it2 = vertex2.connections.iterator();
+			while (it2.hasNext()) {
+				VertexConnection vc = it2.next();
+				if (vc.vertex1 == vertex1 || vc.vertex2 == vertex1) {
+					it2.remove();
+					connections.removeValue(vc, true);
+					break;
+				}
+			}
 		}
 
 		private void remove(Vertex vertex) {
@@ -127,11 +160,12 @@ public class PolygonEditorTest extends BaseScreen {
 				renderer.circle(selected.pos.x, selected.pos.y, .25f, 16);
 			}
 			for (Vertex vertex : vertices) {
-				renderer.setColor(Color.GREEN);
+				renderer.setColor(vertex.color);
 				renderer.circle(vertex.pos.x, vertex.pos.y, .15f, 8);
 			}
 		}
 
+		private Vector2 tmp = new Vector2();
 		public void drawLine(ShapeRenderer renderer) {
 			if (drawGrid) {
 				renderer.setColor(1, 1, 1, .5f);
@@ -150,11 +184,20 @@ public class PolygonEditorTest extends BaseScreen {
 					renderer.line(sx, sy + y * gridY, sx + width * gridX, sy + y * gridY);
 				}
 			}
-			for (Vertex vertex : vertices) {
+			for (VertexConnection vc : connections) {
 				renderer.setColor(0, 1, 1, .5f);
-				for (Vertex other : vertex.connections) {
-					renderer.line(vertex.pos, other.pos);
+				for (int i = 0; i < connections.size; i++) {
+					VertexConnection ovc = connections.get(i);
+					if (vc != ovc) {
+						// skip connections with shared vertices
+						if (vc.vertex1 == ovc.vertex1 || vc.vertex1 == ovc.vertex2 || vc.vertex2 == ovc.vertex1 || vc.vertex2 == ovc.vertex2) continue;
+						if (Intersector.intersectSegments(vc.vertex1.pos, vc.vertex2.pos, ovc.vertex1.pos, ovc.vertex2.pos, tmp)) {
+							renderer.setColor(1, 0, 0, .5f);
+							break;
+						}
+					}
 				}
+				renderer.line(vc.vertex1.pos, vc.vertex2.pos);
 			}
 		}
 
@@ -181,9 +224,15 @@ public class PolygonEditorTest extends BaseScreen {
 				for (Vertex vertex : vertices) {
 					if (vertex.pos.epsilonEquals(x, y, .25f)) {
 						if (link) {
-							if (selected.connections.contains(vertex, true)) {
-								disconnect(selected, vertex);
-							} else {
+							boolean disconnected = false;
+							for (VertexConnection vc : selected.connections) {
+								if (vc.vertex1 == vertex || vc.vertex2 == vertex) {
+									disconnected = true;
+									disconnect(selected, vertex);
+									break;
+								}
+							}
+							if (!disconnected) {
 								connect(selected, vertex);
 							}
 						} else {
@@ -218,8 +267,22 @@ public class PolygonEditorTest extends BaseScreen {
 			if (dragged != null) {
 				bounds.set(0, 0, 0, 0);
 				for(Vertex vertex : vertices) {
+					vertex.color.set(Color.GREEN);
 					extendBounds(vertex.pos.x, vertex.pos.y);
 				}
+				// check for vertex collisions
+				for(Vertex vertex : vertices) {
+					for (int i = 0; i < vertices.size; i++) {
+						Vertex other = vertices.get(i);
+						if (vertex != other) {
+							if (vertex.pos.epsilonEquals(other.pos, VERTEX_OVERLAP_EPSILON)) {
+								vertex.color.set(Color.RED);
+								other.color.set(Color.RED);
+							}
+						}
+					}
+				}
+
 				updateMarginBounds();
 				dragged = null;
 			}
@@ -256,11 +319,23 @@ public class PolygonEditorTest extends BaseScreen {
 		protected static class Vertex {
 			public int id;
 			public Vector2 pos = new Vector2();
-			public Array<Vertex> connections = new Array<>();
+			public Array<VertexConnection> connections = new Array<>();
+			public Color color = new Color(Color.GREEN);
 
 			public Vertex (float x, float y, int id) {
 				pos.set(x, y);
 				this.id = id;
+			}
+		}
+
+		protected static class VertexConnection {
+			public Color color = new Color(Color.GREEN);
+			public Vertex vertex1;
+			public Vertex vertex2;
+
+			public VertexConnection (Vertex vertex1, Vertex vertex2) {
+				this.vertex1 = vertex1;
+				this.vertex2 = vertex2;
 			}
 		}
 	}
