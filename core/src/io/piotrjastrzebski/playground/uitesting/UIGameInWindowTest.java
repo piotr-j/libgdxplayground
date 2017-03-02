92ea6d41cd3e0a59befbf7da3444f1883cd035c2
EvilEntity <me@piotrjastrzebski.io>
2017-03-02 10:46:02 +0100

    use actual test to run in a window

--- core/src/io/piotrjastrzebski/playground/uitesting/UIGameInWindowTest.java
+++ core/src/io/piotrjastrzebski/playground/uitesting/UIGameInWindowTest.java
@@ -1,10 +1,10 @@
 package io.piotrjastrzebski.playground.uitesting;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -13,6 +13,7 @@ import com.kotcrab.vis.ui.widget.VisWindow;
 import io.piotrjastrzebski.playground.BaseScreen;
 import io.piotrjastrzebski.playground.GameReset;
 import io.piotrjastrzebski.playground.PlaygroundGame;
+import io.piotrjastrzebski.playground.simple.Box2dReflectTest;
 
 /**
  * Created by EvilEntity on 25/01/2016.
@@ -28,8 +29,10 @@ public class UIGameInWindowTest extends BaseScreen {
 
 	private VisWindow window;
 	private VisTable container;
-	private OrthographicCamera camera;
-	private ExtendViewport viewport;
+	private OrthographicCamera innerCamera;
+	private InnerExtendViewport innerViewport;
+	private CustomBox2dReflectTest innerView;
+
 	public UIGameInWindowTest (GameReset game) {
 		super(game);
 		window = new VisWindow("WTF?");
@@ -40,50 +43,15 @@ public class UIGameInWindowTest extends BaseScreen {
 		window.add(container).expand().fill().minSize(100, 100);
 		window.setResizable(true);
 
-		camera = new OrthographicCamera();
+		innerView = new CustomBox2dReflectTest(game);
+
+		innerCamera = new OrthographicCamera();
 		// we modify update a bit for it take account for our screenXY not being 0 always
-		viewport = new ExtendViewport(W_SIZE, W_SIZE, camera) {
-			@Override
-			public void update (int screenWidth, int screenHeight, boolean centerCamera) {
-				float minWorldWidth = getMinWorldWidth();
-				float minWorldHeight = getMinWorldHeight();
-				float maxWorldWidth = getMaxWorldWidth();
-				float maxWorldHeight = getMaxWorldHeight();
-				// Fit min size to the screen.
-				float worldWidth = minWorldWidth;
-				float worldHeight = minWorldHeight;
-				Vector2 scaled = Scaling.fit.apply(worldWidth, worldHeight, screenWidth, screenHeight);
+		innerViewport = new InnerExtendViewport(W_SIZE, W_SIZE, innerCamera);
+		innerView.setViewport(innerViewport);
 
-				// Extend in the short direction.
-				int viewportWidth = Math.round(scaled.x);
-				int viewportHeight = Math.round(scaled.y);
-				if (viewportWidth < screenWidth) {
-					float toViewportSpace = viewportHeight / worldHeight;
-					float toWorldSpace = worldHeight / viewportHeight;
-					float lengthen = (screenWidth - viewportWidth) * toWorldSpace;
-					if (maxWorldWidth > 0) lengthen = Math.min(lengthen, maxWorldWidth - minWorldWidth);
-					worldWidth += lengthen;
-					viewportWidth += Math.round(lengthen * toViewportSpace);
-				} else if (viewportHeight < screenHeight) {
-					float toViewportSpace = viewportWidth / worldWidth;
-					float toWorldSpace = worldWidth / viewportWidth;
-					float lengthen = (screenHeight - viewportHeight) * toWorldSpace;
-					if (maxWorldHeight > 0) lengthen = Math.min(lengthen, maxWorldHeight - minWorldHeight);
-					worldHeight += lengthen;
-					viewportHeight += Math.round(lengthen * toViewportSpace);
-				}
-
-				setWorldSize(worldWidth, worldHeight);
-
-				// Center, we added getScreenXY() vs the default extend vp
-				setScreenBounds(
-					getScreenX() + (screenWidth - viewportWidth) / 2,
-					getScreenY() + (screenHeight - viewportHeight) / 2,
-					viewportWidth, viewportHeight);
-
-				apply(centerCamera);
-			}
-		};
+		multiplexer.addProcessor(innerView.getMultiplexer());
+		Gdx.input.setInputProcessor(multiplexer);
 	}
 
 	@Override public void render (float delta) {
@@ -93,11 +61,11 @@ public class UIGameInWindowTest extends BaseScreen {
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		stage.act(delta);
 		stage.draw();
-		renderWindowed();
+		renderWindowed(delta);
 	}
 
 	private Vector2 tmp = new Vector2();
-	private void renderWindowed () {
+	private void renderWindowed (float delta) {
 		// get containers position in world coordinates
 		tmp.set(container.getX(), container.getY());
 		window.localToStageCoordinates(tmp);
@@ -107,25 +75,100 @@ public class UIGameInWindowTest extends BaseScreen {
 		int width = (int)container.getWidth();
 		int height = (int)container.getHeight();
 		// doing all this each frame is a waste, a check for same position/size wouldnt hurt probably
-		viewport.setScreenPosition(x, y);
-		viewport.update(width, height, false);
+		innerViewport.setScreenPosition(x, y);
+		innerViewport.update(width, height, false);
+
+		Gdx.gl.glEnable(GL20.GL_SCISSOR_TEST);
+		innerView.render(delta);
+		Gdx.gl.glDisable(GL20.GL_SCISSOR_TEST);
+
+
 		// draw some stuff, this could be replaced with a screen or whatever
-		renderer.setProjectionMatrix(camera.combined);
-		renderer.begin(ShapeRenderer.ShapeType.Filled);
-		renderer.setColor(Color.CYAN);
-		// large rect bigger then the world size
-		renderer.rect(-5, -5, 10, 10);
-		renderer.setColor(Color.RED);
-		// dot in the middle
-		renderer.circle(-.05f, -.05f, .05f, 16);
-		renderer.end();
-		renderer.begin(ShapeRenderer.ShapeType.Line);
-		renderer.setColor(Color.RED);
-		// world size border
-		renderer.rect(-W_SIZE/2, -W_SIZE/2, W_SIZE, W_SIZE);
-		renderer.end();
-		// need to re apply previous viewport
+//		renderer.setProjectionMatrix(innerCamera.combined);
+//		renderer.begin(ShapeRenderer.ShapeType.Filled);
+//		renderer.setColor(Color.CYAN);
+//		// large rect bigger then the world size
+//		renderer.rect(-5, -5, 10, 10);
+//		renderer.setColor(Color.RED);
+//		// dot in the middle
+//		renderer.circle(-.05f, -.05f, .05f, 16);
+//		renderer.end();
+//		renderer.begin(ShapeRenderer.ShapeType.Line);
+//		renderer.setColor(Color.RED);
+//		// world size border
+//		renderer.rect(-W_SIZE/2, -W_SIZE/2, W_SIZE, W_SIZE);
+//		renderer.end();
+		// need to re apply previous innerViewport
 		guiViewport.apply();
+	}
+
+	@Override public void dispose () {
+		super.dispose();
+		innerView.dispose();
+	}
+
+	private static class CustomBox2dReflectTest extends Box2dReflectTest {
+		public CustomBox2dReflectTest (GameReset game) {
+			super(game);
+		}
+
+		public void setViewport (ExtendViewport viewport) {
+			this.gameViewport = viewport;
+			gameCamera = (OrthographicCamera)viewport.getCamera();
+			gameCamera.zoom = 4;
+			gameCamera.update();
+		}
+
+		public InputMultiplexer getMultiplexer () {
+			return multiplexer;
+		}
+	}
+
+	private static class InnerExtendViewport extends ExtendViewport {
+
+		public InnerExtendViewport (float minWorldWidth, float minWorldHeight, Camera camera) {
+			super(minWorldWidth, minWorldHeight, camera);
+		}
+
+		@Override public void update (int screenWidth, int screenHeight, boolean centerCamera) {
+			float minWorldWidth = getMinWorldWidth();
+			float minWorldHeight = getMinWorldHeight();
+			float maxWorldWidth = getMaxWorldWidth();
+			float maxWorldHeight = getMaxWorldHeight();
+			// Fit min size to the screen.
+			float worldWidth = minWorldWidth;
+			float worldHeight = minWorldHeight;
+			Vector2 scaled = Scaling.fit.apply(worldWidth, worldHeight, screenWidth, screenHeight);
+
+			// Extend in the short direction.
+			int viewportWidth = Math.round(scaled.x);
+			int viewportHeight = Math.round(scaled.y);
+			if (viewportWidth < screenWidth) {
+				float toViewportSpace = viewportHeight / worldHeight;
+				float toWorldSpace = worldHeight / viewportHeight;
+				float lengthen = (screenWidth - viewportWidth) * toWorldSpace;
+				if (maxWorldWidth > 0)
+					lengthen = Math.min(lengthen, maxWorldWidth - minWorldWidth);
+				worldWidth += lengthen;
+				viewportWidth += Math.round(lengthen * toViewportSpace);
+			} else if (viewportHeight < screenHeight) {
+				float toViewportSpace = viewportWidth / worldWidth;
+				float toWorldSpace = worldWidth / viewportWidth;
+				float lengthen = (screenHeight - viewportHeight) * toWorldSpace;
+				if (maxWorldHeight > 0)
+					lengthen = Math.min(lengthen, maxWorldHeight - minWorldHeight);
+				worldHeight += lengthen;
+				viewportHeight += Math.round(lengthen * toViewportSpace);
+			}
+
+			setWorldSize(worldWidth, worldHeight);
+
+			// Center, we added getScreenXY() vs the default extend vp
+			setScreenBounds(getScreenX() + (screenWidth - viewportWidth) / 2, getScreenY() + (screenHeight - viewportHeight) / 2,
+				viewportWidth, viewportHeight);
+
+			apply(centerCamera);
+		}
 	}
 
 	// allow us to start this test directly
