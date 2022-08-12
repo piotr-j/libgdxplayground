3e9f0b19d1a8c3d53f1a3e03f86bd3f09a39c258
PiotrJ <piotr@piotrjastrzebski.io>
2022-08-12 23:29:02 +0200

    multiline test text

--- core/src/io/piotrjastrzebski/playground/uitesting/UITextraTest.java
+++ core/src/io/piotrjastrzebski/playground/uitesting/UITextraTest.java
@@ -11,7 +11,6 @@ import com.github.tommyettinger.textra.TypingLabel;
 import com.kotcrab.vis.ui.widget.VisLabel;
 import io.piotrjastrzebski.playground.BaseScreen;
 import io.piotrjastrzebski.playground.GameReset;
-import io.piotrjastrzebski.playground.PLog;
 import io.piotrjastrzebski.playground.PlaygroundGame;
 
 /**
@@ -26,11 +25,11 @@ public class UITextraTest extends BaseScreen {
 		Font[] fonts = KnownFonts.getAllStandard();
 		BitmapFont[] bitmapFonts = getFonts();
 		Table labels = new Table();
-		labels.defaults().pad(5);
+		labels.defaults().pad(2);
 		for (int i = 0; i < fonts.length; i++) {
 			Font font = fonts[i];
 			labels.add(new VisLabel(font.name)).left();
-			TypingLabel label = new TypingLabel("Dummy Text 123", skin, font);
+			TypingLabel label = new TypingLabel("Dummy Text 123\nAnd some more yy?", skin, font);
 			labels.add(label).expandX().left();
 			label.validate();
 
@@ -39,10 +38,9 @@ public class UITextraTest extends BaseScreen {
 				Label.LabelStyle style = new Label.LabelStyle();
 				style.font = bf;
 				style.fontColor = Color.WHITE;
-				Label bmLabel = new Label("Dummy Text 123", style);
+				Label bmLabel = new Label("Dummy Text 123\nAnd some more yy?", style);
 				bmLabel.validate();
 				float scale = label.getPrefHeight()/bmLabel.getPrefHeight();
-				PLog.log(font.name + ", " + label.getPrefHeight() + ", " + bmLabel.getPrefHeight() + ", " + scale);
 				bmLabel.setFontScale(scale);
 				labels.add(bmLabel).expandX().left();
 			} else {
